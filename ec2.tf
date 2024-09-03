5e2f9d921e9a82dee80bba5bd00b02a669dc9735
benjakunee96 <benbenkuneepong@gmail.com>
2024-09-03 09:46:58 +0700

    false ebs

--- ec2.tf
+++ ec2.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "ec2-instance" {
   root_block_device {
     volume_size           = "100"
     volume_type           = "gp3"
-    delete_on_termination = true
+    delete_on_termination = false
   }
 
   
